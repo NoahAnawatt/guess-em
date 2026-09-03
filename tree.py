3a950a8e1d34d9f44a466c7756a5ccf2b05e4031
NoahAnawatt <59205862+NoahAnawatt@users.noreply.github.com>
2026-09-03 15:41:58 -0500

    One-hot encoded

--- tree.py
+++ tree.py
@@ -6,16 +6,13 @@ import pandas as pd
 dataset = pd.read_csv("Guess_Who_Dataset.csv",header=None)
 dataset = dataset.set_index([0]).transpose()
 
-print(dataset)
-
 x_leg = dataset.drop(columns = "Name")
 y_leg = dataset["Name"]
 
 x = dataset.drop(columns = "Name")
 y = dataset["Name"]
 
-ordinal = OrdinalEncoder()
-x = ordinal.fit_transform(x)
+x = pd.get_dummies(x)
 
 label = LabelEncoder()
 y = label.fit_transform(y)
@@ -23,8 +20,11 @@ y = label.fit_transform(y)
 clf = tree.DecisionTreeClassifier()
 clf = clf.fit(x,y)
 
+print("Data fed to model:")
+print(x)
+
 tree.plot_tree(clf,
-               feature_names = x_leg.columns.tolist(),
+               feature_names = x.columns.tolist(),
                class_names = label.classes_.tolist(),
                filled = True,
                fontsize = 3
